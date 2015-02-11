c456208702491f405e878dc8eb94cadc753e6786
Julian Nadeau <julian@jnadeau.ca>
2015-02-11 14:21:00 -0500

    Update dependency for Snowball, preprocess description

--- src/main/java/models/Ticket.java
+++ src/main/java/models/Ticket.java
@@ -4,16 +4,20 @@ import java.sql.Date;
 import java.util.ArrayList;
 
 import org.apache.mahout.math.Vector;
+import org.tartarus.snowball.ext.EnglishStemmer;
 
 public class Ticket {
 	public int rowId;
 	public int ticketId;
 	public String myShopifyURL;
 	public String subject;
-	public String description;
 	public Date zCreatedAt;
-	private ArrayList<String> tags;
 	public Vector featureVector;
+	
+	public String description;
+	public String processedDescription;
+	
+	private ArrayList<String> tags;
 
 	public Ticket(int rowId, int ticketId, String myShopifyURL, String subject, String description, Date zCreatedAt, String[] tags) {
 		this.rowId = rowId;
@@ -28,6 +32,8 @@ public class Ticket {
 				addTag(tag);
 			}
 		}
+		
+		preprocessDescription();
 	}
 
 	public ArrayList<String> getTags() {
@@ -48,6 +54,26 @@ public class Ticket {
 
 	public void addTag(String tag) {
 		this.tags.add(tag);
+	}
+	
+	private void preprocessDescription() {
+		String preprocessedText = this.description.replaceAll("\\n", " ").replaceAll("[^a-zA-Z ]", "").toLowerCase();
+		preprocessedText = preprocessedText.replace("full chat transcript below all timestamps in utc timezone   ", ""); //Precedes every chat
+		String stemmedText = "";
+		
+		// This will stem the text. EnglishStemmer has a set of default stop words. Will experiment with more later.
+		for (String s : preprocessedText.split(" ")) {
+			EnglishStemmer stemmer = new EnglishStemmer();
+		    stemmer.setCurrent(s);
+		    stemmer.stem();
+		    if (stemmedText.length() > 0) {
+		    	stemmedText += " " + new String(stemmer.getCurrentBuffer());
+		    } else {
+		    	stemmedText += new String(stemmer.getCurrentBuffer());
+		    }
+		}
+	    
+		this.processedDescription = stemmedText;
 	}
 
 }
